ade70f3b2960ae941410df4d9fd2ccaf067ffa8a
Angelo Cisneros <a.e.cisneros@gmail.com>
2015-02-03 21:21:00 -0800

    Refactor into controller module. Getting it to work on local machine.

--- hangman.js
+++ hangman.js
@@ -1,8 +1,10 @@
-var HangManCtrl = function($scope) {
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('HangManCtrl', ['$scope', function($scope) {
   $scope.currentWord = "angular";
   $scope.guesses = [];
-  $scope.guessedStyle = {color: 'green'};
-  $scope.wrongStyle = {color: 'red'};
+  $scope.guessedStyle = {color: 'green', display: 'inline'};
+  $scope.wrongStyle = {color: 'red', display: 'inline'};
 
   $scope.addWord = function(){
       $scope.currentWord = $scope.newWord;
@@ -25,8 +27,6 @@ var HangManCtrl = function($scope) {
     if($scope.guesses.indexOf($scope.newGuess) === -1){
       $scope.guesses.push($scope.newGuess);
     }
-    // $scope.currentGuess = $scope.newGuess;
-    
     //testing
     //console.log($scope.guesses);
     $scope.newGuess = "";
@@ -35,6 +35,6 @@ var HangManCtrl = function($scope) {
   $scope.isGuessed = function(){
     var chars = $scope.currentWord.split("");
     return chars.indexOf(this.guess) !== -1 ? $scope.guessedStyle : $scope.wrongStyle;
-  }
+  };
 
-};
+}]);
